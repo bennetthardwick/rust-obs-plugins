4ec54ce248d1129e2862d59b049be832fd733799
Bennett Hardwick <me@bennetthardwick.com>
2020-03-05 20:42:12 +1000

    Add generated

--- obs-sys/build.rs
+++ obs-sys/build.rs
@@ -1,32 +1,29 @@
 extern crate bindgen;
 
 use std::env;
+use std::fs;
 use std::path::PathBuf;
 
 fn main() {
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
+
+    if let Ok(bindings) = bindgen::Builder::default()
         .header("wrapper.h")
         .blacklist_type("_bindgen_ty_2")
         .derive_default(true)
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+    {
+        bindings
+            .write_to_file(&out_path)
+            .expect("Couldn't write bindings!");
+        fs::copy(&out_path, "generated/bindings.rs").expect("Could not copy bindings!");
+    } else {
+        println!("cargo:warning=Could not find obs headers - using pre-compiled.");
+        println!("cargo:warning=This could result in a library that doesn't work.");
+        fs::copy("generated/bindings.rs", out_path).expect("Could not copy bindings!");
+    }
 }

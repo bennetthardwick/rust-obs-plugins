ec9a9ec0512a274bea956709c2870d4b891d318e
Bennett Hardwick <me@bennetthardwick.com>
2020-11-24 21:45:51 +1000

    Add id to hotkey

--- src/source/hotkey.rs
+++ src/source/hotkey.rs
@@ -1,12 +1,16 @@
-use obs_sys::obs_hotkey_t;
+use obs_sys::{obs_hotkey_get_id, obs_hotkey_id, obs_hotkey_t};
 
 pub struct Hotkey {
     key: *mut obs_hotkey_t,
-    pub pressed: bool
+    pub pressed: bool,
 }
 
 impl Hotkey {
     pub(crate) unsafe fn from_raw(key: *mut obs_hotkey_t, pressed: bool) -> Self {
         Self { key, pressed }
+    }
+
+    pub fn id(&self) -> obs_hotkey_id {
+        unsafe { obs_hotkey_get_id(self.key) }
     }
 }

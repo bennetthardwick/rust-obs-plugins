cbcc3fa5078980f8dbb267bbd8a87247dc89a655
Clouds Flowing <clouds.flowing@gmail.com>
2022-08-08 02:45:54 +0800

    add output context

--- src/output/context.rs
+++ src/output/context.rs
@@ -1,11 +1,17 @@
-use obs_sys::{obs_output_create, obs_output_get_ref, obs_output_release, obs_output_t};
+use std::ffi::CStr;
 
-use crate::{
-    hotkey::{Hotkey, HotkeyCallbacks},
-    prelude::DataObj,
-    string::ObsString,
-    wrapper::PtrWrapper,
+use obs_sys::{
+    obs_output_t, obs_output_create, obs_output_release, obs_output_get_ref, obs_output_start, obs_output_stop,
+    obs_output_set_delay, obs_output_can_pause, obs_output_pause, obs_output_paused,
+    obs_encoder_t, obs_output_set_video_encoder, obs_output_get_video_encoder, obs_output_set_audio_encoder, obs_output_get_audio_encoder,
+    obs_output_can_begin_data_capture, obs_output_begin_data_capture, obs_output_end_data_capture, obs_output_initialize_encoders,
+    obs_output_video, obs_output_audio, obs_output_set_media, obs_output_active, obs_output_get_delay, obs_output_force_stop,
+    obs_output_get_total_bytes, obs_output_get_frames_dropped, obs_output_get_total_frames, obs_enum_outputs, obs_output_get_id, obs_output_get_name,
 };
+
+use crate::hotkey::HotkeyCallbacks;
+use crate::media::{video::VideoRef, audio::AudioRef};
+use crate::{string::ObsString, hotkey::Hotkey, prelude::DataObj, wrapper::PtrWrapper};
 
 /// Context wrapping an OBS output - video / audio elements which are displayed
 /// to the screen.
@@ -32,6 +38,19 @@ impl Clone for OutputContext {
     }
 }
 
+impl Drop for OutputContext {
+    fn drop(&mut self) {
+        unsafe { obs_output_release(self.inner) }
+    }
+}
+
+extern "C" fn enum_proc(params: *mut std::ffi::c_void, output: *mut obs_output_t) -> bool {
+    let mut v = unsafe { Box::<Vec<*mut obs_output_t>>::from_raw(params as *mut _) };
+    v.push(output);
+    Box::into_raw(v);
+    true
+}
+
 impl OutputContext {
     pub fn new(id: ObsString, name: ObsString, settings: Option<DataObj<'_>>) -> Self {
         let settings = match settings {
@@ -44,11 +63,115 @@ impl OutputContext {
 
         unsafe { Self::from_raw(output) }
     }
-}
+    pub fn all_outputs() -> Vec<Self> {
+        let outputs = Vec::<*mut obs_output_t>::new();
+        let params = Box::into_raw(Box::new(outputs));
+        unsafe {
+            obs_enum_outputs(Some(enum_proc), params as *mut _);
+        }
+        let outputs = unsafe { Box::from_raw(params) };
+        outputs.into_iter().map(|i| unsafe { OutputContext::from_raw(i) }).collect()
+    }
 
-impl Drop for OutputContext {
-    fn drop(&mut self) {
-        unsafe { obs_output_release(self.inner) }
+    pub fn output_id(&self) -> Option<&str> {
+        unsafe {
+            let ptr = obs_output_get_id(self.inner);
+            if ptr.is_null() {
+                None
+            } else {
+                Some(CStr::from_ptr(ptr).to_str().unwrap())
+            }
+        }
+    }
+
+    pub fn name(&self) -> Option<&str> {
+        unsafe {
+            let ptr = obs_output_get_name(self.inner);
+            if ptr.is_null() {
+                None
+            } else {
+                Some(CStr::from_ptr(ptr).to_str().unwrap())
+            }
+        }
+    }
+
+    pub fn start(&mut self) -> bool {
+        unsafe { obs_output_start(self.inner) }
+    }
+    pub fn stop(&mut self) {
+        unsafe { obs_output_stop(self.inner) }
+    }
+    pub fn force_stop(&mut self) {
+        unsafe { obs_output_force_stop(self.inner) }
+    }
+    pub fn is_active(&self) -> bool {
+        unsafe { obs_output_active(self.inner) }
+    }
+    pub fn set_delay(&mut self, delay_secs: u32, flags: u32) {
+        unsafe {
+            obs_output_set_delay(self.inner, delay_secs, flags)
+        }
+    }
+    pub fn delay(&self) -> u32 {
+        unsafe { obs_output_get_delay(self.inner) }
+    }
+    pub fn can_pause(&self) -> bool {
+        unsafe { obs_output_can_pause(self.inner) }
+    }
+    pub fn pause(&mut self, pause: bool) -> bool {
+        unsafe { obs_output_pause(self.inner, pause) }
+    }
+    pub fn is_paused(&self) -> bool {
+        unsafe { obs_output_paused(self.inner) }
+    }
+    pub fn set_video_encoder(&mut self, encoder: *mut obs_encoder_t) {
+        unsafe { obs_output_set_video_encoder(self.inner, encoder) }
+    }
+    pub fn video_encoder(&self) -> *mut obs_encoder_t {
+        unsafe { obs_output_get_video_encoder(self.inner) }
+    }
+    pub fn set_audio_encoder(&mut self, encoder: *mut obs_encoder_t, idx: usize) {
+        unsafe { obs_output_set_audio_encoder(self.inner, encoder, idx as _) }
+    }
+    pub fn audio_encoder(&self, idx: usize) -> *mut obs_encoder_t {
+        unsafe { obs_output_get_audio_encoder(self.inner, idx as _) }
+    }
+    pub fn init_encoders(&mut self, flags: u32) -> bool {
+        unsafe { obs_output_initialize_encoders(self.inner, flags) }
+    }
+    pub fn can_start_capture(&self, flags: u32) -> bool {
+        unsafe { obs_output_can_begin_data_capture(self.inner, flags) }
+    }
+    pub fn start_capture(&mut self, flags: u32) -> bool {
+        unsafe { obs_output_begin_data_capture(self.inner, flags) }
+    }
+    pub fn stop_capture(&mut self) {
+        unsafe { obs_output_end_data_capture(self.inner) }
+    }
+
+    pub fn video(&self) -> VideoRef {
+        let video = unsafe { obs_output_video(self.inner) };
+        VideoRef::from_raw(video)
+    }
+    pub fn audio(&self) -> AudioRef {
+        let audio = unsafe { obs_output_audio(self.inner) };
+        AudioRef::from_raw(audio)
+    }
+    pub fn set_media(&mut self, video: VideoRef, audio: AudioRef) {
+        self.set_video_and_audio(video, audio)
+    }
+    pub fn set_video_and_audio(&mut self, video: VideoRef, audio: AudioRef) {
+        unsafe { obs_output_set_media(self.inner, video.pointer, audio.pointer) }
+    }
+
+    pub fn total_bytes(&self) -> u64 {
+        unsafe { obs_output_get_total_bytes(self.inner) }
+    }
+    pub fn frames_dropped(&self) -> u32 {
+        unsafe { obs_output_get_frames_dropped(self.inner) as u32 }
+    }
+    pub fn total_frames(&self) -> u32 {
+        unsafe { obs_output_get_total_frames(self.inner) as u32 }
     }
 }
 

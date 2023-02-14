1f3b82ed2865b83219eee2b2ffd73c9bb68f731d
Shugo Kawamura <shugo_k@flowing.tokyo>
2023-02-14 19:35:09 +0900

    Add OBS Frontend API to wrapper

--- obs-sys/build.rs
+++ obs-sys/build.rs
@@ -29,26 +29,30 @@ fn main() {
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
 
-    if let Ok(bindings) = bindgen::Builder::default()
+    match bindgen::Builder::default()
         .header("wrapper.h")
+        .clang_arg("-I./obs/libobs/")
         .blocklist_type("_bindgen_ty_2")
         .blocklist_type("_bindgen_ty_3")
         .blocklist_type("_bindgen_ty_4")
         .derive_default(true)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-    {
-        bindings
-            .write_to_file(&out_path)
-            .expect("Couldn't write bindings!");
-        fs::copy(&out_path, "generated/bindings.rs").expect("Could not copy bindings!");
-    } else {
-        if env::var("DONT_USE_GENERATED_BINDINGS").is_ok() {
-            panic!("Could not find obs headers - aborting!");
-        }
-
-        println!("cargo:warning=Could not find obs headers - using pre-compiled.");
-        println!("cargo:warning=This could result in a library that doesn't work.");
-        fs::copy("generated/bindings.rs", out_path).expect("Could not copy bindings!");
-    }
+        .generate() {
+            Ok(bindings) => {
+                bindings
+                .write_to_file(&out_path)
+                .expect("Couldn't write bindings!");
+                fs::copy(&out_path, "generated/bindings.rs").expect("Could not copy bindings!");
+            },
+            Err(e) => {
+                println!("cargo:warning={}",e );
+                if env::var("DONT_USE_GENERATED_BINDINGS").is_ok() {
+                    panic!("Could not find obs headers - aborting!");
+                }
+            
+                println!("cargo:warning=Could not find obs headers - using pre-compiled.");
+                println!("cargo:warning=This could result in a library that doesn't work.");
+                fs::copy("generated/bindings.rs", out_path).expect("Could not copy bindings!");
+                    },
+                }
 }
